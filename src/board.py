eeb632e6761d59e973e1e568391b17e83b8806a6
Rocco Fortuna <roccofortuna982@gmail.com>
2020-09-19 14:46:51 +0200

    updated display method of game class for board

--- src/board.py
+++ src/board.py
@@ -3,11 +3,13 @@ Define Game object containing a board to insert Pieces and solve the game.
 """
 
 from typing import List
-
 # define type Piece that is a 2D array (list of lists of ints)
 Piece = List[List[int]]
 
 class PositionException(Exception):
+    pass
+
+class RemovalException(Exception):
     pass
 
 class Game():
@@ -18,7 +20,7 @@ class Game():
     def __init__(self, size: int = 7):
         self.size = size
         # initialize square board
-        self.board = [[0 for _ in range(size)] for _ in range(size)]
+        self.board = [[-1 for _ in range(size)] for _ in range(size)]
 
 
     def is_insertable(self, piece: Piece, y: int, x: int) -> bool:
@@ -50,23 +52,78 @@ class Game():
         # Check if piece does not overlap with other pieces
         for i, row in enumerate(piece):
             for j, element in enumerate(row):
-                if self.board[y+i][x+j] > 0 and piece[y][x] == 1:
+                if self.board[y+i][x+j] > 0 and piece[i][j] == 1:
                     return False
 
         return True
 
+    def insert_piece(self, piece: Piece, y: int, x: int, identifier: str):
+        """Inserts piece in the game board at position (y, x)
 
+        The shape of the piece is inserted in the board by changing
+        the value of the board in the shape of the piece to the identifier
+
+        Identifiers are strings of the form: 'p0'...'p9'
+
+        Args:
+            piece (Piece): the piece to insert on the board
+            y (int): the vertical position at which the piece is inserted
+            x (int): the horizontal position at which the piece is inserted
+        """
+        for i, row in enumerate(piece):
+            for j, element in enumerate(row):
+                if element == 1:
+                    self.board[y+i][x+j] = identifier
+
+    def remove_piece(self, piece: Piece, y: int, x: int, identifier: str):
+        """[summary]
+
+        Args:
+            piece (Piece): the piece to remove from the board
+            y (int): the vertical position from which the piece is removed
+            x (int): the horizontal position from which the piece is removed
+        """
+        for i, row in enumerate(piece):
+            for j, element in enumerate(row):
+                if element == 1:
+                    if self.board[y+i][x+j] != identifier:
+                        raise RemovalException(\
+                            f'Removing inexistent piece:\n{piece}\nfrom \
+                                (y, x) = ({y}, {x}) in board:\n{self.board}')
+                    self.board[y+i][x+j] = 0 # reset board value to empty
 
     def __str__(self):
-        s = ''
+        s = ' '+'__'*self.size+'\n'
         for row in self.board:
-            s += (str(row) + '\n')
-        return s
+            printed_row = '|'
+            for element in row:
+                printed_row += ' ' + str(element) if int(element) >= 0 else ' ' + '-'
+            s += (str(printed_row) + '|\n')
+        return s + ' ' + '¯¯'*self.size
 
 
 if __name__ == "__main__":
+    from pieces import pieces, display_piece
+
     # instantiate game
     g = Game()
 
     # display current board state
     print(g)
+
+    # insert a piece
+    piece_id = 2
+    display_piece(pieces[piece_id])
+    g.insert_piece(pieces[piece_id], 2, 3, f'{piece_id}')
+
+    # display new board state
+    print(g)
+
+    # try to remove piece from wrong position, then remove at right one
+    try:
+        g.remove_piece(pieces[piece_id], 3, 3, f'{piece_id}')
+    except RemovalException as e:
+        print(f'Exception occurred: {e}')
+        print(g)
+        g.remove_piece(pieces[piece_id], 2, 3, f'{piece_id}')
+        print(g)
