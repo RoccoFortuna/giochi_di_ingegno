59d85eb0b7a244dea0457995d7f23dd29d361fe5
Rocco Fortuna <roccofortuna982@gmail.com>
2020-09-20 19:45:04 +0200

    add pruning-allowing method to discard branches impossible to solve

--- src/board.py
+++ src/board.py
@@ -1,8 +1,11 @@
 """
 Define Game object containing a board to insert Pieces and solve the game.
 """
+import copy
 
 from typing import List
+from pieces import display_2Dlist
+
 # define type Piece that is a 2D array (list of lists of ints)
 Piece = List[List[int]]
 
@@ -18,9 +21,54 @@ class Game():
     allows for insertion and removal of pieces.
     """
     def __init__(self, size: int = 7):
-        self.size = size
+        self.board_size = size
         # initialize square board
         self.board = [[-1 for _ in range(size)] for _ in range(size)]
+
+    def is_solvable(self) -> bool:
+        """Check whether self.board does not contain empty islads of a number of blocks
+        that is not divisible by 5.
+
+        This method is used to prune the hopeless branches where <5 empty spaces have
+        been surrounded by other pieces and can therefore not be filled by any 5-squares
+        pieces, or empty islands of for example 9, which cannot be filled by any number
+        of 5-squares pieces. This works with the assumption that the first positioned piece
+        is the only 4-squares piece.
+
+        Returns:
+            bool: True if the board does not contain empty islands of 4 or less spaces
+        """
+        def get_surrounding_coors(board, y: int, x: int):
+            return [(y+i, x+j) for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]\
+                        if y+i >= 0 and y+i < self.board_size and x+j >= 0 and x+j < self.board_size]
+
+        tmp = copy.deepcopy(self.board) # copy the board not to modify it
+        for i in range(len(tmp)):
+            row = tmp[i]
+            for j in range(len(row)):
+                element = row[j]
+                if element == -1:       # if empty space is found
+                    tmp[i][j] = -2      # mark as seen
+                    count = 1           # initialize counter to 1
+                    # list of following coordinates to check
+                    to_discover = get_surrounding_coors(tmp, i, j)
+                    while to_discover:
+                        curr_y, curr_x = to_discover.pop()
+                        if tmp[curr_y][curr_x] == -1: # if island extends, mark as seen and expand to surroundings
+                            tmp[curr_y][curr_x] = -2 # set entry to -2 and expand to_discover
+                            surr_coors = get_surrounding_coors(tmp, curr_y, curr_x)
+                            to_discover.extend(surr_coors)
+                            count += 1
+
+                            # print(surr_coors)
+                            # display_2Dlist(tmp)
+                            # print(count)
+                    if count % 5 != 0:
+                        return False
+        return True
+
+
+
 
 
     def is_insertable(self, piece: Piece, y: int, x: int) -> bool:
@@ -44,15 +92,15 @@ class Game():
         width = len(piece[0])
 
         # Check if piece fits within board boundaries
-        if x + width > self.size: # check if piece exceeds width of board
+        if x + width > self.board_size: # check if piece exceeds width of board
             return False
-        if y + height > self. size: # check if piece exceeds height of board
+        if y + height > self.board_size: # check if piece exceeds height of board
             return False
 
         # Check if piece does not overlap with other pieces
         for i, row in enumerate(piece):
             for j, element in enumerate(row):
-                if self.board[y+i][x+j] > 0 and piece[i][j] == 1:
+                if (not self.board[y+i][x+j] == -1) and (piece[i][j] == 1):
                     return False
 
         return True
@@ -89,17 +137,17 @@ class Game():
                     if self.board[y+i][x+j] != identifier:
                         raise RemovalException(\
                             f'Removing inexistent piece:\n{piece}\nfrom \
-                                (y, x) = ({y}, {x}) in board:\n{self.board}')
-                    self.board[y+i][x+j] = 0 # reset board value to empty
+(y, x) = ({y}, {x}) in board:\n{self}')
+                    self.board[y+i][x+j] = -1 # reset board value to empty
 
     def __str__(self):
-        s = ' '+'__'*self.size+'\n'
+        s = '\n '+'--'*self.board_size+'\n'
         for row in self.board:
             printed_row = '|'
             for element in row:
                 printed_row += ' ' + str(element) if int(element) >= 0 else ' ' + '-'
             s += (str(printed_row) + '|\n')
-        return s + ' ' + '¯¯'*self.size
+        return s + ' ' + '--'*self.board_size
 
 
 if __name__ == "__main__":
